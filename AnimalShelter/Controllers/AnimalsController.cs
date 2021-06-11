b6e509a1e52951f10f82166708e5ee5c7e9f53ea
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-06-11 11:41:11 -0800

    built out AnimalsController and did Migrations

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -1,39 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+using System.Linq;
 
 namespace AnimalShelter.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class AnimalsController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+    public AnimalsController(AnimalShelterContext db)
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+      _db = db;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get(string name, string species, string breed, string description, string gender)
+    {
+      var query = _db.Animals.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+       if (breed != null)
+      {
+        query = query.Where(entry => entry.Breed == breed);
+      }
+       if (description != null)
+      {
+        query = query.Where(entry => entry.Description == description);
+      }
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+
+      return query.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      return animal;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
 }
---
+++ AnimalShelter/Migrations/20210611193952_Initial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(20) CHARACTER SET utf8mb4", maxLength: 20, nullable: false),
+                    Species = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Breed = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Description = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Gender = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Breed", "Description", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "Calico", "Very timid, very beautiful scareddy-cat. Aloofness is result of a bad psychedilic experience. Please be gentle", "Female", "Suzie Cream Cheese", "Cat" },
+                    { 2, 3, "Irish Wolfhound", "A gentle, giant, good boy. Loves snuggles and laying in fields of flowers.", "Male", "Jerry", "Dog" },
+                    { 3, 2, "Maine Coon", "One cool customer, doesn't give a flying fig. Has lots of floofs.", "Male", "King Friday", "Cat" },
+                    { 4, 4, "Pomeranian", "Tough and tiny, this little fuzzball has lots of spunk and sass. Enjoys treats, bellyrubs, and squeaky toys.", "Female", "Pancake", "Dog" },
+                    { 5, 7, "Tuxedo", "Very vocal, very affectionate mama cat just wants all of your love", "Female", "Milk Cow", "Cat" },
+                    { 6, 5, "Husky", "This sweet, magical creature will accompany you through the forest, over mountains, and love you unconditionally.", "Female", "Butterfly", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
